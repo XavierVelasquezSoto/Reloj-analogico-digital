2e8f6748805b103c0e9c3b0c72b1b4475e175f26
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-12-06 04:41:01 +0100

    .

---
+++ TIMERS.md
@@ -0,0 +1,8 @@
+// TIMERS
+// setTimeout() -> Establecer un delay hasta la ejercuión de una función
+// setInterval() -> Repetir la ejercución de una función cada X milisegundos
+
+// const sayHello = () => {
+// console.log("hola")
+// }
+// setTimeout(sayHello,5000)
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+//  TIMERS
+//  setTimeout() -> Establecer un delay hasta la ejercuión de una función
+//  setInterval() -> Repetir la ejercución de una función cada X milisegundos
+
+// const sayHello = () => {
+//   console.log("hola")
+// }
+// setTimeout(sayHello,5000)
+
 const hourDigitalElement = document.getElementById("hour-digital");
 const minuteDigitalElement = document.getElementById("minute-digital");
 
