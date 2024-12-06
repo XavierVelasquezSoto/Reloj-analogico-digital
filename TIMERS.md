0990d5d30c843a22ec625b6ec9a39f1b7b3c33ef
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-12-06 04:55:23 +0100

    notas

--- TIMERS.md
+++ TIMERS.md
@@ -6,3 +6,25 @@
 // console.log("hola")
 // }
 // setTimeout(sayHello,5000)
+
+// DATE
+
+// const date = new Date()
+
+// console.log(date)
+// console.log(date.getDate())Nos devuelve el día en el que nos encontramos.
+// console.log(date.getDay())Nos devuelve el día de la semana en el que nos encontramos. 0 es domingo y 6 sábado
+// console.log(date.getMonth()) Nos devuelve el mes en el que nos encontramos. 0 es enero y 11 diciembre
+// console.log(date.getFullYear()) Nos devuelve el año en el que nos encontramos.
+// console.log(date.getMiliseconds()) Nos devuelve los milisegundos para la hora en la que nos encontramos.
+// console.log(date.getSeconds) Nos devuelve los segundos en los que nos encontramos.
+// console.log(date.getMinutes) Nos devuelve los minutos en los que nos encontramos.
+// console.log(date.getHours) Nos devuelve la hora en la que nos encontramos.
+
+// const date = new Date() "Importante" debe de ir siempre dentro de la función, porque se ejecuta y captura la hora, ya no vuelve a capturar la hora
+// const printClock = () => {
+// const date = new Date();
+// console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+// };
+
+// setInterval(printClock, 1000);
--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,28 @@
 // }
 // setTimeout(sayHello,5000)
 
+//  DATE
+
+// const date = new Date()
+
+// console.log(date)
+// console.log(date.getDate())Nos devuelve el día en el que nos encontramos.
+// console.log(date.getDay())Nos devuelve el día de la semana en el que nos encontramos. 0 es domingo y 6 sábado
+// console.log(date.getMonth()) Nos devuelve el mes en el que nos encontramos. 0 es enero y 11 diciembre
+// console.log(date.getFullYear()) Nos devuelve el año en el que nos encontramos.
+// console.log(date.getMiliseconds()) Nos devuelve los milisegundos para la hora en la que nos encontramos.
+// console.log(date.getSeconds) Nos devuelve los segundos en los que nos encontramos.
+// console.log(date.getMinutes) Nos devuelve los minutos en los que nos encontramos.
+// console.log(date.getHours) Nos devuelve la hora en la que nos encontramos.
+
+// const date = new Date() "Importante" debe de ir siempre dentro de la función, porque se ejecuta y captura la hora, ya no vuelve a capturar la hora
+// const printClock = () => {
+//   const date = new Date();
+//   console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+// };
+
+// setInterval(printClock, 1000);
+
 const hourDigitalElement = document.getElementById("hour-digital");
 const minuteDigitalElement = document.getElementById("minute-digital");
 
@@ -25,7 +47,8 @@ const printClockDigital = () => {
   hourDigitalElement.textContent = `${hour}`;
   minuteDigitalElement.textContent = `${minute}`;
 };
-// setInterval(printClockDigital);
+
+//
 
 const dayOfWeekElement = document.getElementById("day-of-week");
 const dayElement = document.getElementById("day");
@@ -66,7 +89,7 @@ const printClockDigitalDate = () => {
   yearElement.textContent = `${date.getFullYear()}`;
 };
 
-// setInterval(printClockDigitalDate);
+//
 
 const hourHandElement = document.getElementById("hour-hand");
 const minuteHandElement = document.getElementById("minute-hand");
@@ -87,8 +110,6 @@ const printClockAnalog = () => {
   minuteHandElement.style.transform = `rotate(${minuteRotation}deg)`;
   secondHandElement.style.transform = `rotate(${secondRotation}deg)`;
 };
-
-// setInterval(printClockAnalog);
 
 setInterval(() => {
   printClockDigital();
